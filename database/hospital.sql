944a34e7cda6010012acc1ce92e72d6dbf9c154b
17206032 <junmeng.xu@ucdconnect.ie>
2020-04-05 17:42:51 +0800

    edify an attribute of database

--- database/hospital.sql
+++ database/hospital.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `hospital` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `hospital`;
 -- MySQL dump 10.13  Distrib 8.0.17, for Win64 (x86_64)
 --
 -- Host: localhost    Database: hospital
@@ -53,7 +55,7 @@ CREATE TABLE `user` (
   `password` varchar(45) NOT NULL,
   `account` varchar(45) NOT NULL,
   `email` varchar(45) NOT NULL,
-  `phone number` varchar(45) NOT NULL,
+  `phoneNumber` varchar(45) NOT NULL,
   `type` int(11) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `name_UNIQUE` (`username`)
@@ -79,4 +81,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-03-31 11:03:15
+-- Dump completed on 2020-04-05 17:41:22
